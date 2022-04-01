cb6c67e55d137de62c75b300970f5d058574f95c
prakhar-glitch <54439226+prakhar-glitch@users.noreply.github.com>
2022-04-01 23:37:34 +0530

    fixing the bg-img

---
+++ router/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import VendingMachine from './VendingMachine';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <h1>Hellooooooo</h1> */}
+      <VendingMachine/>
+    </div>
+  );
+}
+
+export default App;
---
+++ router/src/VendingMachine.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './VendingMachine.css';
+class VendingMachine extends Component {
+    render() {
+        return (
+            <div className="vendingMachine">
+                {/* <h1>Hellooo</h1> */}
+            </div>
+        );
+    }
+}
+
+export default VendingMachine;
