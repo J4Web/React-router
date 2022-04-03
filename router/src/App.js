3341056386ee49e68ceff16b02646417c81a1709
prakhar-glitch <54439226+prakhar-glitch@users.noreply.github.com>
2022-04-04 02:48:36 +0530

    integrated the 404 page as well just do path='*'

--- router/src/App.js
+++ router/src/App.js
@@ -4,14 +4,32 @@ import { Routes, Route, Link,NavLink } from "react-router-dom";
 // import Chips from './Chips';
 // import Soda from './Soda';
 // import Sardines from './Sardines';
-// import Navbar from './Navbar';
 import UseParams from './UseParams';
+// import Navbar from './Navbar';
+
+function Home() {
+  return (
+    <div><h1>This is for Home Page</h1></div>
+  )
+}
+
+
+function Error() {
+  return (
+    <div>
+      <h1>ITS 404 TIME YEAHHHH BOIIIII</h1>
+    </div>
+  )
+}
+
 function App() {
   return (
     <div className="App">
     {/* <Navbar/> */}
             <Routes>
-            <Route path="/food/:id" element={ <UseParams/>} />
+            <Route path="/" element={<Home/>} />
+            <Route path="/food/:id" element={<UseParams/>} />
+            <Route path="*" element={<Error/>}/>
             </Routes>
     </div>
   );
