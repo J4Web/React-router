d46b94f4854dbbb941c2e84f15d3fc6ac75dff7e
prakhar-glitch <54439226+prakhar-glitch@users.noreply.github.com>
2022-04-06 00:51:52 +0530

    Redirect was replaced by Navigate

--- router/src/UseParams.jsx
+++ router/src/UseParams.jsx
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import FoodForm from './FoodForm';
-import { Routes, Route, useParams, Redirect } from "react-router-dom";
+import { Routes, Route, useParams, Redirect, Navigate } from "react-router-dom";
 
 function UseParams() {
     const {id} = useParams();
     const link=`https://source.unsplash.com/1600x900/?${id}`;
     return (
         <div>
-        {/\d/.test(id)? <Redirect to="/" />:
+        {/\d/.test(id)? <Navigate to="/" replace={true} />:
             (<div>
             <h1>You Searched for... {id}</h1>
             <img src={link} alt={id}/>
